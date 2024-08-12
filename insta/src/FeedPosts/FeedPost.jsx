9e6458060e665d440cfde06a55038d703a315a70
enginmertcan <148966209+enginmertcan@users.noreply.github.com>
2024-08-12 11:10:59 +0300

    Update FeedPost.jsx

--- insta/src/FeedPosts/FeedPost.jsx
+++ insta/src/FeedPosts/FeedPost.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import PostHeader from './PostHeader'
-import { Image,Box } from '@chakra-ui/react'
-import PostFooter from './PostFooter'
-import useGetUserProfileById from '../hooks/useGetUserProfileById'
-function FeedPost({post}) {
-  
-  const {userProfile} =useGetUserProfileById(post.createdBy)
+import React from 'react';
+import PostHeader from './PostHeader';
+import { Image, Box } from '@chakra-ui/react';
+import PostFooter from './PostFooter';
+import useGetUserProfileById from '../hooks/useGetUserProfileById';
+import useLikePost from '../hooks/useLikePost';
+
+function FeedPost({ post }) {
+  const { userProfile } = useGetUserProfileById(post.createdBy);
+  const { handleLikePost, isLiked, likes } = useLikePost(post);
 
   return (
     <>
-        <PostHeader post={post} creatorProfile={userProfile} />
-        <Box my={2} borderRadius={4} overflow={"hidden"}> 
-          <Image src={post.imageURL} alt={"feed post ımg"}/> 
-          </Box>
-        <PostFooter post={post} creatorProfile={userProfile}/>
-        
-
+      <PostHeader post={post} creatorProfile={userProfile} />
+      <Box my={2} borderRadius={4} overflow={"hidden"}>
+        <Image 
+          src={post.imageURL} 
+          alt={"feed post img"}  
+          onDoubleClick={handleLikePost} 
+        /> 
+      </Box>
+      <PostFooter 
+        post={post} 
+        creatorProfile={userProfile}
+        handleLikePost={handleLikePost}  
+        isLiked={isLiked}                
+        likes={likes}                  
+      />
     </>
-  )
+  );
 }
 
-export default FeedPost
+export default FeedPost;
